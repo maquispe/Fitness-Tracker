97de2c6b152c8b92370fb2ddf30909d0e80fe31e
maquispe <62685159+maquispe@users.noreply.github.com>
2020-09-08 16:53:43 -0700

    Fixes API routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -12,80 +12,12 @@ module.exports = app => {
   });
 
   app.get("/api/workouts/range", (req, res) => {
-    const oneWeekAgo = new Date();
-    oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
-    Workout.find({
-      day: {
-        $exists: true,
-      },
-    }, (err, docs) => {
-      const data = [
-        {
-          dayOfWeek: "Monday",
-          dayNumber: 1,
-          totalDuration: 0,
-          totalWeight: 0,
-          exerciseNames: [],
-        },
-        {
-          dayOfWeek: "Tuesday",
-          dayNumber: 2,
-          totalDuration: 0,
-          totalWeight: 0,
-          exerciseNames: [],
-        },
-        {
-          dayOfWeek: "Wednesday",
-          dayNumber: 3,
-          totalDuration: 0,
-          totalWeight: 0,
-          exerciseNames: [],
-        },
-        {
-          dayOfWeek: "Thursday",
-          dayNumber: 4,
-          totalDuration: 0,
-          totalWeight: 0,
-          exerciseNames: [],
-        },
-        {
-          dayOfWeek: "Friday",
-          dayNumber: 5,
-          totalDuration: 0,
-          totalWeight: 0,
-          exerciseNames: [],
-        },
-        {
-          dayOfWeek: "Saturday",
-          dayNumber: 6,
-          totalDuration: 0,
-          totalWeight: 0,
-          exerciseNames: [],
-        },
-        {
-          dayOfWeek: "Sunday",
-          dayNumber: 7,
-          totalDuration: 0,
-          totalWeight: 0,
-          exerciseNames: [],
-        },
-      ];
-
-      docs.forEach((workout) => {
-        let dayNumber = workout.day.getDay();
-        if (dayNumber === 0) dayNumber = 7;
-        data[dayNumber - 1].totalDuration += workout.totalDuration;
-        data[dayNumber - 1].totalWeight += workout.totalWeight;
-        workout.exercises.forEach((exercise) => {
-          data[dayNumber - 1].exerciseNames.push(exercise.name);
-        });
-      });
-
-      const toDay = new Date();
-      if (data[6].dayNumber !== toDay.getDay()) {
-        data.unshift(data.pop());
-      }
-      return res.json(data);
+    Workout.find({}).limit(7).then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      console.log(err);
+      res.json(err);
     });
   });
 
